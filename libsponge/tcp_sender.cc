a00c0ca4c37abbd804f19e7339d326778fd61638
joiy908 <joiy9088@gmail.com>
2022-12-01 11:25:21 +0800

    finish lab3

--- libsponge/tcp_sender.cc
+++ libsponge/tcp_sender.cc
@@ -9,8 +9,8 @@
 // For Lab 3, please replace with a real implementation that passes the
 // automated checks run by `make check_lab3`.
 
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+//template <typename... Targs>
+//void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
@@ -18,21 +18,72 @@ using namespace std;
 //! \param[in] retx_timeout the initial amount of time to wait before retransmitting the oldest outstanding segment
 //! \param[in] fixed_isn the Initial Sequence Number to use, if set (otherwise uses a random ISN)
 TCPSender::TCPSender(const size_t capacity, const uint16_t retx_timeout, const std::optional<WrappingInt32> fixed_isn)
-    : _isn(fixed_isn.value_or(WrappingInt32{random_device()()}))
-    , _initial_retransmission_timeout{retx_timeout}
+    : timer{retx_timeout}
+    , _isn(fixed_isn.value_or(WrappingInt32{random_device()()}))
     , _stream(capacity) {}
 
-uint64_t TCPSender::bytes_in_flight() const { return {}; }
+uint64_t TCPSender::bytes_in_flight() const { return unAckQueue.bytesInFlight(); }
 
-void TCPSender::fill_window() {}
+void TCPSender::fill_window() {
+    if (unAckQueue._finAcked || (unAckQueue._finSent && !unAckQueue._finAcked)) {
+        return ;
+    }
+    if (!unAckQueue._synSent) {
+        firstPush(makeSynSeg());
+        return ;
+    }
+    if (!unAckQueue._finSent && _stream.eof() && remainSpace() > 1) {
+        firstPush(makeFirstFinSeg());
+    }
+    if (unAckQueue._synSent && !unAckQueue._synAcked) {
+        return ;
+    }
+
+    while(remainSpace()>0 && hasContentToPush()) {
+        TCPSegment seg = makePayLoadSeg();
+        unAckQueue.push(seg);
+        firstPush(std::move(seg));
+    }
+}
 
 //! \param ackno The remote receiver's ackno (acknowledgment number)
 //! \param window_size The remote receiver's advertised window size
-void TCPSender::ack_received(const WrappingInt32 ackno, const uint16_t window_size) { DUMMY_CODE(ackno, window_size); }
+void TCPSender::ack_received(const WrappingInt32 ackno, const uint16_t window_size) {
+    setWindowSize(window_size);
+
+    // the index of the last
+    // reassembled byte as the checkpoint.
+    uint64_t absAckno = unwrap(ackno, _isn, unAckQueue._baseSqn);
+    if ((unAckQueue._finSent && absAckno > unAckQueue.ackForFinSqn()) ||
+        absAckno <= 0 || absAckno < unAckQueue._baseSqn) {
+        return ;
+    }
+    if(absAckno > unAckQueue._baseSqn) { // expected ack received
+        unAckQueue.updateBase(absAckno);
+        if (!unAckQueue.empty() || (unAckQueue._finSent && !unAckQueue._finAcked))
+            timer.startTimer();
+        else timer.stopTimer();
+    }
+    // ignore other ack
+}
 
 //! \param[in] ms_since_last_tick the number of milliseconds since the last call to this method
-void TCPSender::tick(const size_t ms_since_last_tick) { DUMMY_CODE(ms_since_last_tick); }
+void TCPSender::tick(const size_t ms_since_last_tick) {
+    if (timer._isTimerRunning) {
+        timer._msSinceBaseSegSent += ms_since_last_tick;
+        if (timer._msSinceBaseSegSent >= timer._timeout) {
+            // handler timer
+            timeoutRetransmit();
+        }
+    }
+}
 
-unsigned int TCPSender::consecutive_retransmissions() const { return {}; }
+unsigned int TCPSender::consecutive_retransmissions() const { return timer._consecutive_retransmissions; }
 
-void TCPSender::send_empty_segment() {}
+// only called when generate empty RST seg
+void TCPSender::send_empty_segment() {
+    TCPSegment seg;
+
+    seg.header().seqno = wrap(unAckQueue._next_seqno, _isn);
+    _segments_out.push(std::move(seg));
+}
--- libsponge/tcp_sender.hh
+++ libsponge/tcp_sender.hh
@@ -8,6 +8,7 @@
 
 #include <functional>
 #include <queue>
+#include <deque>
 
 //! \brief The "sender" part of a TCP implementation.
 
@@ -17,22 +18,290 @@
 //! segments if the retransmission timer expires.
 class TCPSender {
   private:
+    // helper class
+    class UnAckQueue {
+      private:
+        // un-acknowledged bytes
+        std::deque<char> _unAcked{};
+        // un-acknowledged seg size
+        // {absSqn, payload.size()
+        std::deque<size_t> _unAckedPayloadSize{};
+
+      public:
+
+        // === about flag
+        // does SYN have been sent
+        bool _synSent{false};
+        bool _synAcked{false};
+        bool _finSent{false};
+        bool _finAcked{false};
+
+        // === about win
+        // the absolute sqn for base
+        uint64_t _baseSqn{0};
+        //! the (absolute) sequence number for the next byte to be sent
+        uint64_t _next_seqno{0};
+
+        uint64_t _win_size{1};
+
+        // assume _finSent == True
+        uint64_t ackForFinSqn() const { return _next_seqno; }
+
+        //! assume _finSent is True !
+        uint64_t ackSqnForFin() const { return _next_seqno; }
+
+        size_t bytesInFlight() const { return _next_seqno - _baseSqn; }
+
+        // if queue is empty, not count SYN and FIN !
+        bool empty() const { return _unAcked.empty(); }
+
+        // assume seg is firstPushed, and != SYNSeg and seg is not empty
+        void push(TCPSegment seg) {
+            // ignore seg.header().fin
+            // copy to window and push out
+            std::string_view payload = seg.payload().str();
+            _unAcked.insert(_unAcked.end(), payload.begin(), payload.end());
+            _unAckedPayloadSize.emplace_back(payload.size());
+        }
+
+        // called by recv_ack
+        // assume:  1 < ack < finSqn, ack > baseSqn
+        TCPSegment frontSeg(const WrappingInt32 isn) const {
+            TCPSegment seg;
+            seg.header().seqno = wrap(_baseSqn, isn);
+            const size_t toSend = _unAckedPayloadSize.front();
+
+            std::string payload(_unAcked.begin(), _unAcked.begin()+toSend);
+            seg.payload() = std::move(payload);
+            if (_finSent && ackSqnForFin()-1 == _baseSqn+toSend) {
+                // fin was sent and seg is last seg
+                seg.header().fin = true;
+            }
+            return seg;
+        }
+
+        // assume  ack <= _nextSqn
+        void updateBase(uint64_t ackno) {
+            if (ackno <= _baseSqn)
+                return;
+            else if (ackno == 1) {
+                _synAcked = true;
+                ++_baseSqn;
+                return;
+            } else if (_finSent && ackno == ackSqnForFin()) {
+                _finAcked = true;
+
+                if (!empty())
+                    erase(ackno);
+                else {
+                    ++_baseSqn;
+                    return;
+                }
+
+            } else {
+                if (!empty())
+                    erase(ackno);
+            }
+        }
+
+        // assume !empty and 1 < ack < ackForFin
+        void erase(uint64_t ackno) {
+            // handle ack-for-FIN
+            if (_finSent && ackno == ackSqnForFin()){
+                _baseSqn = ackno;
+                _unAcked.clear();
+                _unAckedPayloadSize.clear();
+                return ;
+            }
+            // else
+            size_t bytesToErase = 0;
+            auto sizeIt = _unAckedPayloadSize.begin();
+            while(sizeIt != _unAckedPayloadSize.end()) {
+                auto size = *sizeIt;
+                size_t nextSqn = _baseSqn + size;
+                if (nextSqn == ackno) {
+                    bytesToErase += size;
+                    _unAckedPayloadSize.erase(_unAckedPayloadSize.begin(), ++sizeIt);
+                    _baseSqn = ackno;
+                    break ;
+                } else if(nextSqn < ackno) {
+                    bytesToErase += size;
+                    _baseSqn = nextSqn;
+                    ++sizeIt;
+                    continue ;
+                } else { // nextSqn < ack: hard to handler
+                    auto bytesAcked = ackno - _baseSqn;
+                    bytesToErase += bytesAcked;
+                    _baseSqn = ackno;
+                    *sizeIt = size - bytesAcked;
+                    break ;
+                }
+            }
+            _unAcked.erase(_unAcked.begin(), _unAcked.begin()+bytesToErase);
+        }
+
+        // not contain emptyFinSeg, call only by check valid duplicate ack
+        size_t size() const { return _unAckedPayloadSize.size(); }
+    };
+
+    struct Timer {
+        explicit Timer(unsigned int initial_timeout) :
+            _initial_retransmission_timeout{initial_timeout}, _timeout{initial_timeout} {}
+
+        //! retransmission timer for the connection
+        const unsigned int _initial_retransmission_timeout;
+        unsigned int _timeout;
+
+        bool _isTimerRunning{false};
+        // time elapses since last basePacket sent
+        size_t _msSinceBaseSegSent{0};
+        unsigned int _consecutive_retransmissions{0};
+        // false if receive win=0
+        bool _doubleTimeout{true};
+
+        // call by first push
+        void startTimer() {
+            _isTimerRunning = true;
+            _msSinceBaseSegSent = 0;
+            _consecutive_retransmissions = 0;
+            _timeout = _initial_retransmission_timeout;
+        }
+        // call by recv_ack
+        void stopTimer() {
+            startTimer();
+            _isTimerRunning = false;
+        }
+    };
+
+
+    Timer timer;
+
     //! our initial sequence number, the number for our SYN.
     WrappingInt32 _isn;
-
-    //! outbound queue of segments that the TCPSender wants sent
-    std::queue<TCPSegment> _segments_out{};
-
-    //! retransmission timer for the connection
-    unsigned int _initial_retransmission_timeout;
 
     //! outgoing stream of bytes that have not yet been sent
     ByteStream _stream;
 
-    //! the (absolute) sequence number for the next byte to be sent
-    uint64_t _next_seqno{0};
+    //! outbound queue of segments that the TCPSender wants sent
+    std::queue<TCPSegment> _segments_out{};
+
+    //=== pushHelpers : handle timer
+
+    // the seg never been pushed before
+    void firstPush(TCPSegment && seg) {
+        _segments_out.push(std::move(seg));
+        if (!timer._isTimerRunning)
+            timer.startTimer();
+    }
+
+    void timeoutRetransmit(){
+        if (unAckQueue._finAcked) {
+            return ;
+        }
+        // 1. find last send seg and push
+        TCPSegment seg;
+        if(unAckQueue._finSent && !unAckQueue._finAcked) {
+            if (unAckQueue.empty())
+                seg = makeNotFirstFinSeg();
+            else seg = unAckQueue.frontSeg(_isn);
+        } else if (unAckQueue._synSent && !unAckQueue._synAcked) {
+            seg = makeSynSeg();
+        } else if (!unAckQueue.empty()) {
+            seg = unAckQueue.frontSeg(_isn);
+        }
+        _segments_out.push(seg);
+        // 2. handle timer
+        if (timer._doubleTimeout) {
+            timer._timeout *= 2;
+        }
+        timer._msSinceBaseSegSent = 0;
+        ++timer._consecutive_retransmissions;
+    }
+
+
+    // empty seg maker
+    // call by first push, or retransmit.
+    TCPSegment makeSynSeg() {
+        TCPSegment seg;
+        TCPHeader &h = seg.header();
+        h.seqno = wrap(0, _isn);
+
+        h.syn = true;
+
+        if (!unAckQueue._synSent) {
+            unAckQueue._synSent = true;
+            ++unAckQueue._next_seqno;
+        }
+
+        if(_stream.eof() && remainSpace() >= 2) {
+            h.fin = true;
+            unAckQueue._finSent = true;
+            ++unAckQueue._next_seqno;
+        }
+        return seg;
+    }
+
+    // called by fill_will
+    // assume !unAckQueue._finSent && !_stream.eof() && remainSpace() > 1
+    TCPSegment makeFirstFinSeg() {
+        TCPSegment seg;
+
+        seg.header().seqno = wrap(unAckQueue._next_seqno, _isn);
+        seg.header().fin = true;
+        ++unAckQueue._next_seqno;
+        unAckQueue._finSent = true;
+        return seg;
+    }
+
+    // called by recv_ack, to retransmit Seg
+    // called by timeoutRetransmit, to ~
+    // assume unAckQueue._finSent && absAckno == unAckQueue.ackForFinSqn()-1
+    TCPSegment makeNotFirstFinSeg() const {
+        TCPSegment seg;
+        TCPHeader &h = seg.header();
+        h.fin = true;
+        h.seqno = wrap(unAckQueue.ackSqnForFin()-1, _isn);
+        return seg;
+    }
+
+
+    // not count syn
+    bool hasContentToPush() const {
+        return _stream.buffer_size() > 0 ||
+               (_stream.eof() && !unAckQueue._finSent);
+    }
+
+    // assume remainSpace()>0 && hasContentToPush() && !unAckQueue._finSent
+    TCPSegment makePayLoadSeg() {
+        TCPSegment seg;
+        TCPHeader &h = seg.header();
+        h.seqno = wrap(unAckQueue._next_seqno, _isn);
+
+        size_t toSend = std::min(remainSpace(), TCPConfig::MAX_PAYLOAD_SIZE);
+        std::string payload = _stream.read(toSend);
+
+
+        // check if need carry FIN
+        if(_stream.eof() && remainSpace() - payload.size() > 0) {
+            seg.header().fin = true;
+
+            unAckQueue._finSent = true;
+            ++unAckQueue._next_seqno;
+            // now _next_seqno eq ack-for-fin
+        }
+        // change after remainSpace() is called
+        unAckQueue._next_seqno += payload.size();
+        seg.payload() = std::move(payload);
+        return seg;
+    }
+
+    void setWindowSize(const uint16_t size) {
+        timer._doubleTimeout = size != 0;
+        unAckQueue._win_size = size > 0 ? size : 1;
+    }
 
   public:
+    UnAckQueue unAckQueue{};
     //! Initialize a TCPSender
     TCPSender(const size_t capacity = TCPConfig::DEFAULT_CAPACITY,
               const uint16_t retx_timeout = TCPConfig::TIMEOUT_DFLT,
@@ -43,6 +312,16 @@ class TCPSender {
     ByteStream &stream_in() { return _stream; }
     const ByteStream &stream_in() const { return _stream; }
     //!@}
+
+
+    // helper method
+    size_t remainSpace() const {
+        return unAckQueue._win_size - unAckQueue.bytesInFlight();
+    }
+
+    bool finAcked() const {
+        return unAckQueue._finAcked;
+    }
 
     //! \name Methods that can cause the TCPSender to send a segment
     //!@{
@@ -82,10 +361,10 @@ class TCPSender {
     //!@{
 
     //! \brief absolute seqno for the next byte to be sent
-    uint64_t next_seqno_absolute() const { return _next_seqno; }
+    uint64_t next_seqno_absolute() const { return unAckQueue._next_seqno; }
 
     //! \brief relative seqno for the next byte to be sent
-    WrappingInt32 next_seqno() const { return wrap(_next_seqno, _isn); }
+    WrappingInt32 next_seqno() const { return wrap(unAckQueue._next_seqno, _isn); }
     //!@}
 };
 
